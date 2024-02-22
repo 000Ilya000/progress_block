a1ae20058cc8182aa7690fb6ce099a5e2997ebc5
Ilya <ilya.kurymshin@steiza.com>
2024-02-22 16:37:45 +0300

    feature js/html/css
    Добавил анимацию и поправил функционал

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # progress_block
 прототип блока progress для использования в мобильных web-приложениях
+
+
+# ⚡️Prototype of a progress block for use in mobile web applications!
+![image](https://github.com/000Ilya000/progress_block/assets/92045360/192d0d0a-fe73-4583-aef3-f018cf508ed7)
+---
+
+### :man_technologist: progress block:
+
+In this repository I made a block that shows progress depending on value and has amimete valuesand hide
+
+---
--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,68 @@
+// Объявление переменных
 const toggleAnimate = document.querySelector('.toggleAnimate');
 let toggleAnimateActive = false;
-
 const toggleHide = document.querySelector('.toggleHide');
 let toggleHideActive = false;
-
 const progress = document.querySelector('.progress');
 const progressComplite = document.querySelector('.progressFill');
-
 const valueInput = document.querySelector('.valueInput');
+let intervalId;
 
-const maxLength = 3; // Максимальная длина
-
+// Ф-ция объявляющая обновление прогресса(value)
 function updateProgress(value) {
     progress.style.setProperty('--progress-value', value);
     progressComplite.style.setProperty('stroke', value !== 0 ? '#005CFF' : '#EAF3F6');
-    progressComplite.classList.toggle('progressFillHide', value === 0);
 }
 
+// Ф-ция анимации 
+function animateFillProgress(value) {
+    updateProgress(0);
+    setTimeout(() => {
+        updateProgress(value);
+    }, 600);
+}
+
+// Обработчик событий при изменении value
 valueInput.addEventListener('input', (event) => {
-    let value = event.target.value.slice(0, maxLength); // Обрезаем значение до максимальной длины
+    clearInterval(intervalId); // Очищаем предыдущий интервал
+    let value = event.target.value.slice(0, 3); // Обрезаем значение до максимальной длины
     value = Math.max(0, Math.min(100, value)); // Ограничиваем значение от 0 до 100
     updateProgress(value);
     event.target.value = value; // Устанавливаем значение поля ввода
+
+    // Запускаем новый интервал, если активирована анимация
+    if (toggleAnimateActive) {
+        intervalId = setInterval(() => {
+            animateFillProgress(valueInput.value);
+        }, 1700);
+    }
 });
 
+// Обработчик тогла animate
 toggleAnimate.addEventListener('click', () => {
     toggleAnimateActive = !toggleAnimateActive;
     toggleAnimate.classList.toggle('activeAnimate', toggleAnimateActive);
-    console.log(`animate - ${toggleAnimateActive}`);
+    clearInterval(intervalId); // Очищаем текущий интервал
+    if (toggleAnimateActive) {
+        animateFillProgress(valueInput.value);
+        // Устанавливаем новый интервал для анимации
+        intervalId = setInterval(() => {
+            animateFillProgress(valueInput.value);
+        }, 1700);
+    } else {
+        updateProgress(valueInput.value);
+    }
 });
 
+// Обработчик тогла hide
 toggleHide.addEventListener('click', () => {
     toggleHideActive = !toggleHideActive;
-    toggleHide.classList.toggle('activeAnimate', toggleHideActive);
+    toggleHide.classList.toggle('activeHide', toggleHideActive);
     progressComplite.classList.toggle('progressFillHide', toggleHideActive);
     console.log(`hide - ${toggleHideActive}`);
 });
 
+// Обновление value при загрузки контента
 document.addEventListener('DOMContentLoaded', () => {
     updateProgress(valueInput.value);
 });
